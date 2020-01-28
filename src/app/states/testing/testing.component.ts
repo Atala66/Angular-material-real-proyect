5bfbfbac75eab3d8c67bcb6c5481a1fc4ec979ba
Guiomar <guioguguloxilox@gmail.com>
2020-01-28 21:16:23 +0100

    playing with classes

--- src/app/states/testing/testing.component.ts
+++ src/app/states/testing/testing.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./testing.component.less']
 })
 export class TestingComponent implements OnInit {
+  active: string;
+  isRed: boolean;
+  isActive: boolean;
+  isFocused: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.active = 'font-size';
+    this.isRed = false;
+    this.isActive = true;
+    this.isFocused = false;
   }
 
 }
